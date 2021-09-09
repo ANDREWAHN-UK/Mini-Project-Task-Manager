465d860b65075ffeee4c3c332fcc7e2e285648d4
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-09-09 10:28:27 +0000

    app.py - updated imports and hello()

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-  
+
 import os
-from flask import Flask
+from flask import(
+    Flask, flash, render_template,
+     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -13,10 +15,13 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/get_tasks")
 def hello():
-    return "Hello World ... again!"
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
