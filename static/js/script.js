bdd6692901b05f7ba1425077ed3d102d34ef7d01
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-09-17 11:18:52 +0000

    script.js - added jquery functionality for form select

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function () {
   });
   $('.collapsible').collapsible();
   $('.datepicker').datepicker({
-      format: "dd mmmm, yyyy",
-      yearRange: 3,
-      showClearBtn: true,
-      i18n:{
-        done:"Select"
-      }
+    format: "dd mmmm, yyyy",
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: "Select"
+    }
   });
+  $('select').formSelect();
 });
